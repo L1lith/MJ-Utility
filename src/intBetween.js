9eafc0103b33cb3219bbf817e26230e815167f00
L1lith <lily@l1lith.com>
2023-01-08 14:08:03 -0700

    add link

--- src/intBetween.js
+++ src/intBetween.js
@@ -1,4 +1,4 @@
-// Thanks to this legendary post
+// Thanks to this legendary post https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript/7228322#7228322
 export default function intBetween(min = 0, max = 1) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
