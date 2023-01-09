ee86ae272b39dc4f4e154c2207546b1d11ec1330
L1lith <lily@l1lith.com>
2023-01-08 17:08:34 -0700

    fix the setweight function

--- src/setWeight.js
+++ src/setWeight.js
@@ -1,5 +1,7 @@
 export default function setWeight(str, weight = 1) {
   if (typeof str != "string") throw new Error("Expected a string");
+  if (str.includes(","))
+    throw new Error("Prompt cannot contain a comma for this operation");
   if (!isFinite(weight)) throw new Error("Weight is not a valid number");
 
   const [query] = str.split("::");
