ab21aa273b910f09fc9edd6ec8593f141ecdb31c
L1lith <lily@l1lith.com>
2023-01-10 12:32:43 -0700

    add weight randomization

--- src/scripts/randomWordScramble.js
+++ src/scripts/randomWordScramble.js
@@ -3,6 +3,7 @@ import setWeight from "../utils/setWeight.js";
 
 const subPrompts = 45;
 const wordCount = 4;
+const decimals = 2;
 
 async function randomWordScramble() {
   let prompts = [];
@@ -11,8 +12,11 @@ async function randomWordScramble() {
   for (let i = 1; i <= subPrompts; i++) {
     const isPositive = (positive = !positive);
     const done = randomWordPrompt(wordCount);
+    const weight =
+      Math.round(Math.random() * Math.pow(10, decimals)) /
+      Math.pow(10, decimals);
     done.then((prompt) => {
-      prompts.push(setWeight(prompt, isPositive ? 1 : -1));
+      prompts.push(setWeight(prompt, isPositive ? weight : -weight));
     });
     waitList.push(done);
   }
