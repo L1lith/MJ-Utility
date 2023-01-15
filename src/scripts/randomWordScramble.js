aac0bc319ebbd188048b090bc5db449652e8d1ce
L1lith <lily@l1lith.com>
2023-01-14 21:47:52 -0700

    update some stuff

--- src/scripts/randomWordScramble.js
+++ src/scripts/randomWordScramble.js
@@ -1,12 +1,17 @@
 import randomWordPrompt from "../utils/randomWordPrompt.js";
 import setWeight from "../utils/setWeight.js";
 import intBetween from "../utils/intBetween.js";
+import escapeQuote from "../utils/escapeQuote.js";
 
 const subPrompts = Math.round(Math.random() * 30);
 const maxWordCount = 8;
 const decimals = 2;
-const basePrompt = "";
-const flags = "--chaos 10";
+const preprompt =
+  "A toaster experiencing the sensation of taste as it bites into a slice of blue bread + the bread representing the unknown and the impossible + the toaster representing the human desire for exploration and discovery + the blue representing the infinite possibilities of the universe + a sense of wonder and curiosity";
+const basePrompt = escapeQuote(
+  "a beautiful Alaskan mountain devastated by mining + a giant split in the middle from industrial mining activity"
+);
+const flags = "--chaos 25"; //"--chaos 10";
 
 async function randomWordScramble() {
   let prompts = [];
@@ -31,9 +36,11 @@ async function randomWordScramble() {
   await Promise.all(waitList);
   const output = prompts.join("+"); // + " --chaos 10";
   console.log(
-    `~ Stats ~\nAbsolute Weight: ${absWeight}\nNet Weight: ${netWeight}\n\nYour Prompt:\n${basePrompt}::${
+    `~ Stats ~\nAbsolute Weight: ${absWeight}\nNet Weight: ${netWeight}\n\nYour Prompt:\n${
+      preprompt ? preprompt + " " : ""
+    }${basePrompt.replaceAll(" ", "")}::${
       Math.round(absWeight * 100) / 100
-    },${output} ${flags}`
+    },${output} ${flags}`.trim()
   );
 }
 
--- src/utils/randomWordPrompt.js
+++ src/utils/randomWordPrompt.js
@@ -1,14 +1,14 @@
 import Rwg from "random-word-generator";
-import censor from "../utils/censor.js";
-import escapeQuote from "../utils/escapeQuote.js";
+import censor from "./censor.js";
+import escapeQuote from "./escapeQuote.js";
 
 export default async function randomWordPrompt(length = 10) {
   if (!isFinite(length) || length < 1) throw new Error("Invalid Length");
   const generator = new Rwg();
 
   let prompt = [];
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < length / 10; i++) {
     prompt.push(generator.generate());
   }
-  return escapeQuote(await censor(prompt.join(" ")));
+  return escapeQuote(await censor(prompt.join("").substring(0, length)));
 }
